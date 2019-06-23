182cd333871c661d56e5b91750726dec728751fd
yangxiaozong <yangzong_9958@126.com>
2019-06-23 10:59:52 +0800

    跨域

--- user-shiro/src/main/java/bdhb/usershiro/configuration/AuthInterceptor.java
+++ user-shiro/src/main/java/bdhb/usershiro/configuration/AuthInterceptor.java
@@ -10,7 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -98,6 +102,25 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
 		return sysUserEntity;
 
+	}
+
+	private Boolean httpCoressDomain = true;
+
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+		corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+		corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		if (httpCoressDomain) {
+			source.registerCorsConfiguration("/**", buildConfig()); // 4
+		}
+		return new CorsFilter(source);
 	}
 
 }
